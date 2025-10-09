e1bd7e8aff4c2b3884ad86980852718d72409def
duckton <phamductoan144@gmail.com>
2025-10-09 20:40:43 +0700

    update

--- Demo.js
+++ Demo.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World from Node.js via ngrok');
 });
 
-const port = 3000;
-app.listen(port);
-console.log(`Server is listening on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
+console.log(`Server is listening on port ${PORT}`);
