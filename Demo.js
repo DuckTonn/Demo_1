dfc14ff4bc4bd46d6b94f429136948e4f5831706
DuckTonn <phamductoan144@gmail.com>
2025-10-10 12:05:15 +0700

    Update Demo.js

--- Demo.js
+++ Demo.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World from Node.js via ngrok');
+  res.send('Hello World!');
 });
 
 const PORT = process.env.PORT || 3000;
